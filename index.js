864684f5ef9ee3e9960212efde8da9b42312ac84
git_username <shabanshuaib454@gmail.com>
2022-11-19 11:48:14 +0300

    done assignment

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Write your solution in this file!
+const employee = {
+    name: "Shuaib Shaban",
+    streetAddress: "0TC"
+}
+
+
+function updateEmployeeWithKeyAndValue(obj, key, value){
+  return {
+    ...obj,
+    [key] : value
+  }
+
+}
+console.log(updateEmployeeWithKeyAndValue(employee, "Shaban", 454))
+console.log(employee);
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    obj[key] = value
+    return obj
+}
+destructivelyUpdateEmployeeWithKeyAndValue(employee, "shaban", 456)
+console.log(employee);
+
+function deleteFromEmployeeByKey(obj, key){
+    const newObj = {
+        ...obj
+    }
+    delete newObj[key]
+
+    return newObj
+
+}
+
+console.log(deleteFromEmployeeByKey(employee, "jobId"))
+console.log(employee);
+
+function destructivelyDeleteFromEmployeeByKey(obj, key){
+    delete obj[key]
+    return obj
+}
+
+destructivelyDeleteFromEmployeeByKey(employee, "jobId")
+console.log(employee);
